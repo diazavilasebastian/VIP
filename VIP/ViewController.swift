cb01f93810ceb8372b177fabba979fcdbb9697be
sebastian <sebastiaan.diaz.avila@accenture.com>
2020-06-11 14:58:05 -0400

    Inital commit

---
+++ VIP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VIP
+//
+//  Created by ITAU on 11-06-20.
+//  Copyright © 2020 iOSDemos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
